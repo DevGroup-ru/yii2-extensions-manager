073a80f9a84a097a31b4df9f8967afc865bb2078
Pavel Naumov <naumov@devgroup.ru>
2016-03-25 11:01:13 +0300

    config

--- tests/testapp/config/db.php
+++ tests/testapp/config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yii2_extmanager',
     'username' => 'root',
-    'password' => 'winston',
+    'password' => '',
     'charset' => 'utf8',
     'enableSchemaCache' => true,
 ];
